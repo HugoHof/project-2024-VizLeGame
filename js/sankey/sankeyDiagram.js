c069900fba655cd81391732931a1743948da3056
Eric Kurmann <61149724+HoLuDyEr@users.noreply.github.com>
2024-05-30 23:00:07 +0200

    Sankey final (#10)
    * add sankey html items
    
    * pdate play button function call
    
    * add first draft sankey diagram
    
    * link tool box to graph
    
    * imporve toolbox for graph
    
    * finish sankey
    
    * update darker color when selected
    
    * update font family
    
    * add text when no game match filter
    
    * add graph title in right panel
    
    * update date link
    
    * add clear all button
    
    * fix game card strings
    
    * add top k games
    
    * change clear all + toolbox
    
    * adjust select items bar
    
    * fix

---
+++ js/sankey/sankeyDiagram.js
@@ -0,0 +1,382 @@
+
+var unitsSankey = "Games";
+
+var marginSankey = {top: 10, right: 10, bottom: 10, left: 10},
+    widthSankey = 1200 - marginSankey.left - marginSankey.right,
+    heightSankey = 740 - marginSankey.top - marginSankey.bottom;
+
+// format variables
+var formatNumber = d3.format(",.0f"),    // zero decimal places
+    format = function(d) { return formatNumber(d) + " " + unitsSankey; },
+    color = d3.scaleOrdinal(d3.schemeCategory10);
+
+// to know if the graph is currently play
+var isPlayingSankey = false;
+
+var allSankeyGames;
+
+var sankeyFilterList = {
+    genres: [],
+    platforms: [],
+    developers: [],
+};
+
+var sankeyDiagram = d3.select("#sankey_diagram_graph").append("svg")
+    .attr("viewBox", `0 0 ${widthSankey + marginSankey.left + marginSankey.right} ${heightSankey + marginSankey.top + marginSankey.bottom}`)
+    .attr("preserveAspectRatio", "xMidYMid meet")
+    .style("width", "100%")
+    .style("height", "100%")
+    .style("display", "block")
+    .append("g")
+    .attr("transform", "translate(" + marginSankey.left + "," + marginSankey.top + ")");
+
+// Create the tooltip div
+var tooltipSankey = d3.select("#sankey_diagram_graph").append("div")
+  .attr("class", "tooltipGraph");
+
+var sankey = d3.sankey()
+.nodeWidth(36)
+.nodePadding(10)
+.size([widthSankey, heightSankey]);
+
+var path = sankey.link();
+
+d3.json("data/games_by_year_by_all.json", function(error, data) {
+    if (error) throw error;
+    
+    // initial graph draw
+    drawSankey(data);
+
+    // retrieve the slider of the years
+    var sliderOutputSankey = document.getElementById("year_sankey_displayed");
+    document.getElementById("year_sankey_slider")
+        .oninput = function() {
+            // display the year currently displayed
+            document.getElementById("sankey_graph_title").innerHTML = "Number of Video Games Realeased in " + this.value;
+            sliderOutputSankey.innerHTML = this.value;
+            drawListGamesSankey(data, allSankeyGames)
+        };
+
+    // retrieve the selectors of the number of bars displayed
+    document.getElementById("nb_sankey_displayed_selector_genre")
+        .oninput = function() {
+            drawSankey(data);
+        };
+
+    document.getElementById("nb_sankey_displayed_selector_platform")
+        .oninput = function() {
+            drawSankey(data);
+        };
+
+    document.getElementById("nb_sankey_displayed_selector_developer")
+        .oninput = function() {
+            drawSankey(data);
+        };
+
+    // load all data for right panel
+    d3.csv("data/games_data_clean.csv", function(e, allData) {
+        if(e) {
+            console.log('Error when loading the data for interactive diagram : ', e);
+            return;
+        }
+    
+        // initial draw
+        drawListGamesSankey(data, allData);
+    
+        // retrieve the selectors
+        document.getElementById("sankey_list_games_criteria")
+            .oninput = function() {
+                drawListGamesSankey(data, allData);
+            }
+    
+        document.getElementById("sankey_evolution_criteria")
+            .oninput = function() {
+                drawSankeyEvolutionChart(allData);
+            }
+
+        document.getElementById("sankey_list_games_top")
+            .oninput = function() {
+                drawListGamesSankey(data, allData);
+            }
+        
+        allSankeyGames = allData;
+    });
+
+
+    // order draggable
+    const container = document.getElementById('sankey_order_draggable');
+    let draggedElement = null;
+
+    container.addEventListener('dragstart', (e) => {
+        draggedElement = e.target;
+        e.dataTransfer.effectAllowed = 'move';
+    });
+
+    container.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'move';
+
+        const target = e.target;
+        if (target && target !== draggedElement && target.classList.contains('draggable')) {
+            const rect = target.getBoundingClientRect();
+            const threshold = rect.height * 0.5;
+            const mouseOverTop = e.clientY - rect.top;
+            const mouseOverBot = rect.bottom - e.clientY;
+            const prev = mouseOverTop > threshold;
+            const next = mouseOverBot > threshold;
+            container.insertBefore(draggedElement, next && target.nextSibling || prev && target.previousSibling || target);
+            drawSankey(data);
+        }
+    });
+
+    container.addEventListener('drop', (e) => {
+        e.preventDefault();
+        draggedElement = null;
+    });
+
+    container.addEventListener('dragend', (e) => {
+        e.preventDefault();
+        draggedElement = null;
+    });
+
+    // upadte every 3 s the displaying of the graph if isPlayingSankey is activated
+    setInterval(function() { return upadteGraphDisplayinyOnPlaySankey(data); }, 3000);
+
+});
+
+function clickOnItemSankey(data,name) {
+    let lastChar = name.charAt(name.length - 1);
+    if (lastChar === 'g') {
+        updateSankeyFilterList(sankeyFilterList.genres, name);
+    }
+    if (lastChar === 'p') {
+        updateSankeyFilterList(sankeyFilterList.platforms, name);
+    }
+    if (lastChar === 'd') {
+        updateSankeyFilterList(sankeyFilterList.developers, name);
+    }
+
+    drawListGamesSankey(data, allSankeyGames);
+
+    function updateSankeyFilterList(filterList, name) {
+        let ind = filterList.findIndex(item => item === name.slice(0, -1));
+        if (ind == -1) {
+            filterList.push(name.slice(0, -1));
+        } else {
+            filterList.splice(ind, 1);
+        }
+    }
+
+}
+
+// update when time is playing
+function upadteGraphDisplayinyOnPlaySankey(data){
+    const currentYear = Number(document.getElementById("year_sankey_slider").value)
+    if(isPlayingSankey) {
+        if(currentYear >= document.getElementById("year_sankey_slider").max-1) {
+            isPlayingSankey = false
+            document.getElementById("sankey_play_button").innerText = "Play"
+        }
+        // to evict the bug when moveing the side bar during the animation.
+        if(currentYear >= document.getElementById("year_sankey_slider").min && currentYear <= document.getElementById("year_sankey_slider").max - 1) {
+            document.getElementById("year_sankey_displayed").innerHTML = currentYear + 1
+            document.getElementById("year_sankey_slider").value = currentYear + 1
+            document.getElementById("sankey_graph_title").innerHTML = "Number of Video Games Realeased in " + String(currentYear + 1);
+            // re draw the graph
+            drawListGamesSankey(data, allSankeyGames)
+        }
+    }
+}
+
+// Called by play button
+function onClickPlayButtonSankey() {
+    let sankeySlider = document.getElementById("year_sankey_slider");
+    if(sankeySlider.value < sankeySlider.max || isPlayingSankey) {
+        isPlayingSankey  = !isPlayingSankey;
+        document.getElementById("sankey_play_button").innerText = isPlayingSankey ? "Pause" : "Play";
+    }
+}
+
+// check if the filter is selected
+function checkFilterSelected(name) {
+    let lastChar = name.charAt(name.length - 1);
+    if (lastChar === 'g') {
+        return sankeyFilterList.genres.findIndex(item => item === name.slice(0, -1)) >= 0
+    }
+    if (lastChar === 'p') {
+        return sankeyFilterList.platforms.findIndex(item => item === name.slice(0, -1)) >= 0
+    }
+    if (lastChar === 'd') {
+        return sankeyFilterList.developers.findIndex(item => item === name.slice(0, -1)) >= 0
+    }
+    return false;
+}
+
+function drawSankey(data){
+
+    // Clear the previous diagram
+    sankeyDiagram.selectAll("*").remove();
+
+    let sankey_data = data[String(document.getElementById("year_sankey_slider").value)].sort((a, b) => b.value - a.value);
+
+    var nodes = [];
+    var links = [];
+
+    let nbGenre = document.getElementById("nb_sankey_displayed_selector_genre").value;
+    let nbPlatform = document.getElementById("nb_sankey_displayed_selector_platform").value;
+    let nbDeveloper = document.getElementById("nb_sankey_displayed_selector_developer").value;
+
+    // create nodes and links
+    sankey_data.forEach(link => {
+        
+        // update text to avoid loop
+        const genre = link.Genre + 'g';
+        const platform = link.Platform + 'p';
+        const developer = link.Developer + 'd';
+
+        // nodes -------------------------
+        if (nodes.findIndex(it => it.name === genre) == -1 && nbGenre > 0) {
+            nbGenre--;
+            nodes.push({"name": genre, "color": [102, 92, 190]});
+        }
+
+        if (nodes.findIndex(it => it.name === platform) == -1 && nbPlatform > 0) {
+            nbPlatform--;
+            nodes.push({"name": platform, "color": [228, 0, 15]});
+        }
+
+        if (nodes.findIndex(it => it.name === developer) == -1 && nbDeveloper > 0) {
+            nbDeveloper--;
+            nodes.push({"name": developer, "color": [14, 122, 13]});
+        }
+
+        // order of the bars
+        const container = document.getElementById('sankey_order_draggable');
+        const order = Array.from(container.children).map(div => div.id).map(id => {
+            if (id === "sankey_genres") return genre;
+            if (id === "sankey_platforms") return platform;
+            if (id === "sankey_developers") return developer;
+        });
+
+        // links -------------------------
+        // Create two links for each data element
+        if (nodes.findIndex(it => it.name === order[0]) != -1 && nodes.findIndex(it => it.name === order[1]) != -1) {
+            let link1 = { "source": order[0], "target": order[1], "value": link.value };
+            updateOrAddLink(link1);
+        }
+        
+        if (nodes.findIndex(it => it.name === order[1]) != -1 && nodes.findIndex(it => it.name === order[2]) != -1) {
+            let link2 = { "source": order[1], "target": order[2], "value": link.value };
+            updateOrAddLink(link2);
+        }
+
+        // Function to find an existing link and update the value if found
+        function updateOrAddLink(newLink) {
+            const existingLink = links.find(link => link.source === newLink.source && link.target === newLink.target);
+            if (existingLink) {
+                existingLink.value += newLink.value;
+            } else {
+                links.push(newLink);
+            }
+        }
+
+    });
+
+    // loop through each link replacing the text with its index from node
+    links.forEach(function (d, i) {
+        links[i].source = nodes.findIndex(item => item.name === links[i].source);
+        links[i].target = nodes.findIndex(item => item.name === links[i].target);
+    });
+
+    // now loop through each nodes to make nodes an array of objects
+    // rather than an array of strings
+    nodes.forEach(function (d, i) {
+        nodes[i] = { "name": d };
+    });
+
+    sankey
+        .nodes(nodes)
+        .links(links)
+        .layout(8);
+
+    var link = sankeyDiagram.append("g").selectAll(".link")
+        .data(links)
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("d", path)
+        .style("stroke-width", function(d) { return Math.max(2, d.dy); })
+        .sort(function(a, b) { return b.dy - a.dy; });
+
+    link.on("mouseover", function(d) {
+            tooltipSankey.transition().duration(200).style("opacity", .9);
+            tooltipSankey.html(d.source.name.name.slice(0, -1) + " → " + d.target.name.name.slice(0, -1) + "<br/>" + format(d.value))
+                .style("left", (d3.event.pageX + 5) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            tooltipSankey.transition().duration(500).style("opacity", 0);
+        });
+
+    var node = sankeyDiagram.append("g").selectAll(".node")
+        .data(nodes)
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .call(d3.drag()
+            .on("start", function(event) { 
+                startX = event.x;
+                startY = event.y;
+                this.parentNode.appendChild(this); 
+            })
+            .on("drag", dragmove)
+            .on("end", function(event) {
+                const endX = event.x;
+                const endY = event.y;
+                const dx = endX - startX;
+                const dy = endY - startY;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+                const clickThreshold = 5;
+        
+                if (distance < clickThreshold) {
+                    clickOnItemSankey(data, event.name.name)
+                }
+            }));
+        
+
+    node.append("rect")
+        .attr("height", function(d) { return d.dy; })
+        .attr("width", sankey.nodeWidth())
+        .style("fill", function(d) { return checkFilterSelected(d.name.name) ? d3.rgb(...d.name.color).darker(2) : d3.rgb(...d.name.color);})
+        .style("stroke", function(d) { return d3.rgb(...d.name.color).darker(2); })
+        .on("mouseover", function(d) {
+            tooltipSankey.transition().duration(200).style("opacity", .9);
+            tooltipSankey.html(d.name.name.slice(0, -1) + "<br/>" + format(d.value))
+                .style("left", (d3.event.pageX + 5) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            tooltipSankey.transition().duration(500).style("opacity", 0);
+        });
+
+    node.append("text")
+        .attr("x", -6)
+        .attr("y", function(d) { return d.dy / 2; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", "end")
+        .attr("transform", null)
+        .text(function(d) { return d.name.name.slice(0, -1); })
+        .filter(function(d) { return d.x < widthSankey / 2; })
+        .attr("x", 6 + sankey.nodeWidth())
+        .attr("text-anchor", "start");
+
+    function dragmove(d) {
+        d3.select(this).attr("transform", "translate(" + (
+            d.x = Math.max(0, Math.min(widthSankey - d.dx, d3.event.x))
+        ) + "," + (
+            d.y = Math.max(0, Math.min(heightSankey - d.dy, d3.event.y))
+        ) + ")");
+        sankey.relayout();
+        link.attr("d", path);
+    }
+
+}
---
+++ js/sankey/sankeyRightPanel.js
@@ -0,0 +1,145 @@
+// sorting function
+function sortSankey(data, sortField) {
+    return data.sort(function(a, b) {
+        return b[sortField] - a[sortField];
+    });
+}
+
+function filterSankey(data, filterField, filterList) {
+    return data.filter(elem => {
+        let include = true;
+        filterList.forEach(filter => {
+            include &= elem[filterField].includes(filter)
+        })
+        return include;
+    });
+}
+
+let sankey_data;
+
+//Draw the list of the games relative to a developer.
+async function drawListGamesSankey(sankeyData, data) {
+
+    sankey_data = sankeyData;
+    
+    // edit the dropdown list---------------------------------------------------------------
+    let selectedItems = [];
+    selectedItems = selectedItems.concat(sankeyFilterList.genres, sankeyFilterList.developers, sankeyFilterList.platforms);
+    let selectedText;
+    
+    if (selectedItems.length > 0) {
+        selectedText = selectedItems.join(', ');
+    } else {
+        selectedText = "All games";
+    }
+
+    let filterText = `
+        <div class="custom-select">
+            <div class="selected-item" onclick="toggleDropdownSankey()">
+                <div class="selected-text"><span class="selected-text-main">Selected items :</span> ${selectedText}</div>
+                <span class="arrow down">></span>
+            </div>
+            <div class="custom-select-content">
+    `;
+
+    sankeyFilterList.genres.forEach(filter => {
+        filterText += `<div>${filter}<span class="remove-btn" onclick="removeFilterSankey('genres', '${filter}')">x</span></div>`;
+    });
+    sankeyFilterList.developers.forEach(filter => {
+        filterText += `<div>${filter}<span class="remove-btn" onclick="removeFilterSankey('developers', '${filter}')">x</span></div>`;
+    });
+    sankeyFilterList.platforms.forEach(filter => {
+        filterText += `<div>${filter}<span class="remove-btn" onclick="removeFilterSankey('platforms', '${filter}')">x</span></div>`;
+    });
+
+    filterText += '</div></div>';
+
+    document.getElementById("sankey_list_games_filter").innerHTML = filterText;
+
+    let gameList = data;
+
+    // filter by genre--------------------------------------------------------------------------
+    if (sankeyFilterList.genres.length > 0)
+        gameList = filterSankey(gameList, 'Genres', sankeyFilterList.genres);
+
+    // filter by platform---------------------------------------------------------------------------
+    if (sankeyFilterList.platforms.length > 0)
+        gameList = filterSankey(gameList, 'Platforms', sankeyFilterList.platforms);
+
+    // filter by developer-------------------------------------------------------------------------
+    if (sankeyFilterList.developers.length > 0)
+        gameList = filterSankey(gameList, 'Developers', sankeyFilterList.developers);
+
+    let gameListCard = gameList;
+    
+    // filter by year--------------------------------------------------------------------------
+    gameListCard = gameListCard.filter(function(a) {
+        return document.getElementById("year_sankey_slider").value == new Date(a.Release_Date).getFullYear();
+    });
+    
+    // sort by criteria----------------------------------------------------------------------------
+    gameListCard = sortSankey(gameListCard, String(document.getElementById("sankey_list_games_criteria").value));
+
+    let textHTML = "";
+
+    if (gameListCard.length <= 0) {
+        textHTML += '<h1 class="gameCardTitle" style="width: 100%;" >No game matching the current filter(s) and date</h1>';
+    }
+
+    function listStringToString(list) {
+        return list.match(/"(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*'/g).join(", ").replace(/["']/g, '');
+    }
+
+    // only take top k
+    gameListCard = gameListCard.slice(0, document.getElementById("sankey_list_games_top").value);
+
+    // create a card game for all the games
+    for(let game of gameListCard) {
+        textHTML += '<div class="gameCard gameCardHorizontalFlex">'
+        textHTML += '<div class= "gameCardTitleContainer">'
+        textHTML += '<h1 class="gameCardTitle">' + game["Title"] + '</h1>'
+        textHTML += '</div>'
+        textHTML += '<div class= "gameDescriptionContainer">'
+        textHTML += '<label class="gameCardLabel">Genre(s) </label>'
+        textHTML += '<p class="gameCardTextElem">' + listStringToString(game["Genres"]) + '</p>'
+        textHTML += '<label class="gameCardLabel">Developer(s) </label>'
+        textHTML += '<p class="gameCardTextElem">' + listStringToString(game["Developers"]) + '</p>'
+        textHTML += '<label class="gameCardLabel">Platform(s) </label>'
+        textHTML += '<p class="gameCardTextElem">' + listStringToString(game["Platforms"]) + '</p>'
+        textHTML += '</div>'
+        textHTML += '</div>'
+    }
+    document.getElementById("sankey_list_games").innerHTML = textHTML;
+
+
+    // draw graph ---------------------------------------------------------------------------------------
+    drawSankeyEvolutionChart(gameList);
+    drawSankey(sankeyData);
+
+}
+
+// remove the filter, called from the dropdownmenu ---------------------------------------------------------------------
+function removeFilterSankey(type, filter) {
+    if (type === 'genres') {
+        sankeyFilterList.genres = sankeyFilterList.genres.filter(item => item !== filter);
+    } else if (type === 'developers') {
+        sankeyFilterList.developers = sankeyFilterList.developers.filter(item => item !== filter);
+    } else if (type === 'platforms') {
+        sankeyFilterList.platforms = sankeyFilterList.platforms.filter(item => item !== filter);
+    } else if (type === 'all'){
+        sankeyFilterList.genres = [];
+        sankeyFilterList.platforms = [];
+        sankeyFilterList.developers = [];
+    }
+    // redraw
+    drawListGamesSankey(sankey_data, allSankeyGames);
+}
+
+// toggle the dropdown called form the dropdownmenu
+function toggleDropdownSankey() {
+    const content = document.querySelector(".custom-select-content");
+    const arrow = document.querySelector(".arrow");
+    content.classList.toggle("show");
+    arrow.classList.toggle("down");
+    arrow.classList.toggle("up");
+}
